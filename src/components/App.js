858164740f100e421ffb5801ebf99b57fd876cd7
4MN35T <tanmaykarmore01@gmail.com>
2023-04-09 19:51:02 +0530

    just help me

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
-import React ,{useState, useRef} from 'react'
-import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
+import React, { useState, useRef } from "react";
+import "../styles/App.css";
+import PortalButton from "./PortalButton.js";
+import PortalTextArea from "./PortalTextArea.js";
 
-//complete the function 
+//complete the function
 const App = () => {
-const inpref = useRef(null);
-const [val, setVal] = useState("");
-const changeText = () => {
-  setVal(inpref.current.value);
-  inpref.current.value = "";
-};
-
+  const inpref = useRef(null);
+  const [val, setVal] = useState("");
+  const changeText = () => {
+    setVal(inpref.current.value);
+    inpref.current.value = "";
+  };
 
   return (
-    
     <div id="main">
-      <input id="input" ref={inpref}></input><br/>
-      <PortalButton buttonclick={changeText} /><br/>
-      <PortalTextArea value={val}/>
+      <input id="input" ref={inpref}></input>
+      <br />
+      <PortalButton buttonclick={changeText} />
+      <br />
+      <PortalTextArea valu={val} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //complete this function using portals
-const PortalButton=(props)=>{
-    return (
-        <div>
-            <button id="button" onClick={() => {props.buttonclick();}}>Click</button>
-        </div>,
-        document.getElementById("portal-button")
-    )
-}
+const PortalButton = (props) => {
+  return ReactDOM.createPortal(
+    <div>
+      <button
+        id="button"
+        onClick={() => {
+          props.buttonclick();
+        }}
+      >
+        Click
+      </button>
+    </div>,
+    document.getElementById("portal-button"),
+  );
+};
 export default PortalButton;
-
-//portal-button id will be used here for portal purpose
